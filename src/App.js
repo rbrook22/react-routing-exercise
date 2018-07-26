bde0d76d6f0b1c9d90e6d2ae7bb559488d9c9162
Richard Brooks <rickbrooks22@gmail.com>
2018-07-26 11:02:44 -0400

    imported route from react router dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Route} from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -10,9 +11,12 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="">Puppies</a></li>
+        </ul>
+        
       </div>
     );
   }
